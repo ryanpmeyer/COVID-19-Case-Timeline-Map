edcc22ab0dce432ec9820426de9722ee4a632307
Ryan Meyer <ryanmeyerstuff@gmail.com>
2020-12-11 19:15:10 -0800

    updates

--- website/map_script.js
+++ website/map_script.js
@@ -1,16 +1,30 @@
 const mapboxAccessToken ='pk.eyJ1IjoicnlteTIwMjAiLCJhIjoiY2tmc29ndnk0MGJlazJ5bTZlNDNwaW5hNiJ9.n8Rd5g1O2mvWG_LFdSLhTA';
-const caseDataFile = "COVID_CASES.json";
-const deathDataFile = "COVID_DEATHS.json";
-const countiesGeoFile = "geojson-counties-fips.json";
-var caseData = new dfd.DataFrame(caseDataJSON);
-var deathData = new dfd.DataFrame(deathDataJSON);
-var countiesGeo= new dfd.DataFrame(countiesGeoJSON);
-var map = L.map('map-cases').setView([37.8, -96], 4);
+const caseDataFile = "https://raw.githubusercontent.com/ryanpmeyer/Geog80Final/master/data/COVID_CASES.csv";
+const deathDataFile = "https://raw.githubusercontent.com/ryanpmeyer/Geog80Final/master/data/COVID_DEATHS.csv";
+const countiesGeoFile = "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json";
+var caseData;
+var deathData;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
-    id: 'mapbox/light-v9',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(map);
+init();
 
-L.geoJson(countiesGeo).addTo(map);
+async function init() {
+    console.log("loading data");
+    let caseDataPromise = dfd.read_csv(caseDataFile);
+    let deathDataPromise = dfd.read_csv(deathDataFile);
+    caseData = await caseDataPromise;
+    deathData = await deathDataPromise;
+    console.log(caseData);
+    createMap()
+}
+
+function createMap() {
+    var map = L.map('map-cases').setView([37.8, -96], 4);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+        id: 'mapbox/light-v9',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(map);
+    let geoJSON = JSON.parse(countiesGeoFile);
+    L.geoJson(countiesGeoFile).addTo(map);
+}
